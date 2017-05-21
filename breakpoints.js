1f39b761d3f55e9291501358f27e1ac8bb94df9a
Jeff Li <jeff@bbdata.ca>
2017-05-21 00:02:04 -0700

    v1 of my attempt of creating a breakpoints.js that can be used to build UI functionality that changes at different breakpoints.

--- breakpoints.js
+++ breakpoints.js
@@ -2,31 +2,135 @@
 'use strict';
   
   // Constructor
-  function Breakpoints(options) {
-    this.options = Breakpoints.extend({}, Breakpoints.defaults, options);
-    this.debug = {
-      calls: 0, // Keep track of the number of times handleResize() is called for debugging.
+  function Breakpoints(config, queued) {
+    var variableName = config.variableName;
+
+    if (typeof variableName !== 'string' && !variableName.length) {
+      throw new Error('Missing variableName.');
     }
     
+    this.config = Breakpoints.extend({}, Breakpoints.defaults, config);
     this.currentBreakpoint = null;
     this.previousBreakpoint = null;
     this.handlers = {};
     
-    // Sort the breakpoints in DESC order
-    this.options.breakpoints = this.options.breakpoints.sort(function(a, b) {
+    // Backup the existing queue of functions.
+    this.q = queued;
+    
+    this.debug = {
+      calls: 0, // Keep track of the number of times handleBreakpoint() is called for debugging.
+    }
+    
+    // Sort the breakpoints in DESC order.
+    this.config.breakpoints = this.config.breakpoints.sort(function(a, b) {
       return (b-a);
     });
     
-    // Create a name for each breakpoint handler.
-    for (var i in this.options.breakpoints) {
-      var breakpoint = this.options.breakpoints[i],
-          eventName = 'breakpoint' + breakpoint;
-          
-      this.handlers['breakpoint' + breakpoint] = [];
+    // Create name for each breakpoint handler.
+    for (var i in this.config.breakpoints) {
+      var breakpoint = this.config.breakpoints[i],
+          handlerName = variableName + breakpoint;
+      
+      this.handlers[handlerName] = [];
     }
   }
   
-  // Public
+  // Initialization
+  Breakpoints.prototype.init = function() {
+    // Backup the existing queue of functions.
+    var queued = this.q;
+
+    // Replace the push function with one that executes the function passed to it.
+    this.q.push = function () {
+      for (var i = 0; i < arguments.length; i++) {
+        try {
+          if (typeof arguments[i] === "function") {
+            arguments[i]();
+          }
+          else {
+            // What to do when given something not a function.
+          }
+        }
+        catch (e) {
+          // Error handling.
+        }
+      }
+    }
+    
+    // Execute all the queued functions.
+    // apply() turns the array into individual arguments.
+    this.q.push.apply(this.q, queued);
+
+    var variableName = this.config.variableName;
+    
+    // Attach resize listener to window.
+    window.addEventListener('resize', function() {
+      // Clear the timeout.
+      clearTimeout(window[variableName].config.timeout);
+      
+      // Start timeout to wait for resize event to stop.
+      window[variableName].config.timeout = setTimeout(function(){
+        window[variableName].handleBreakpoint();
+      }, window[variableName].config.delay);
+    });
+  }
+
+  // Callback for window resize event.
+  Breakpoints.prototype.handleBreakpoint = function() {
+    this.debug.calls += 1;
+
+    var width = Breakpoints.getDeviceWidth(),
+        breakpoints = this.config.breakpoints;
+
+    // Find which breakpoint we're in.
+    for (var i in breakpoints) {
+      if (width >= breakpoints[i]) {
+        this.currentBreakpoint = breakpoints[i];
+        break;
+      }
+    }
+
+    // Fire off the handler for this breakpoint.
+    if (this.currentBreakpoint != this.previousBreakpoint) {
+      this.fireBreakpoint(this.config.variableName + this.currentBreakpoint);
+      this.previousBreakpoint = this.currentBreakpoint;
+    }
+  };
+  
+  // Method to add breakpoint handlers.
+  Breakpoints.prototype.addBreakpointHandler = 
+  Breakpoints.prototype.addEventListener = function(event, fn) {
+    this.handlers[event].push(fn);
+  };
+  
+  // Method to fire breakpoint handlers.
+  Breakpoints.prototype.fireBreakpoint = function(event){
+    var args = [].slice.call(arguments, 1), 
+        callbacks = this.handlers[event];
+  
+    if (callbacks) {
+      callbacks = callbacks.slice(0);
+      for (var i = 0, len = callbacks.length; i < len; ++i) {
+        callbacks[i].apply(this, args);
+      }
+    }
+  };
+
+  // Default config
+  Breakpoints.defaults = {
+    breakpoints: [0, 768, 992, 1200],
+    // Strng name of the variable that will store Breakpoints. This is used
+    // during initialization so windows.onResize can find where Breakpoints
+    // is without having to hardcode it into the object. Responsibility
+    // goes to where Breakpoints is implemented.
+    variableName: null,
+    // Store timeout id.
+    timeout: false,
+    // Time to wait before the resize event is stopped.
+    delay: 50,
+  };
+  
+  // Private
   Breakpoints.getDeviceWidth = function() {
     /**
      * Some libraries like jQuery and Google Publisher Tag uses a normalized document 
@@ -45,33 +149,11 @@
      */
     return window.innerWidth || (document.documentElement || document.body).clientWidth;
   };
-
-  // Callback for window resize event.
-  Breakpoints.prototype.handleResize = function() {
-    this.debug.calls += 1;
-    
-    var width = Breakpoints.getDeviceWidth(),
-        breakpoints = this.options.breakpoints;
-    
-    // Loop through and find out which breakpoint we're in.
-    for (var i in breakpoints) {
-      if (width >= breakpoints[i]) {
-        this.currentBreakpoint = breakpoints[i];
-        break;
-      }
-    }
-    
-    // Fire off the functions for this breakpoint.
-    if (this.currentBreakpoint != this.previousBreakpoint) {
-      this.previousBreakpoint = this.currentBreakpoint;
-      this.fireBreakpoint('breakpoint' + this.currentBreakpoint);
-    }
-  };
   
   // Private
   Breakpoints.extend = function() {
     var args = Array.prototype.slice.call(arguments);
-  
+
     function merge(target, obj) {
       if (typeof target === 'object' && typeof obj === 'object') {
         for (var key in obj) {
@@ -80,70 +162,15 @@
           }
         }
       }
-  
+
       return target;
     }
-  
+
     for (var i = 1, end = args.length; i < end; i++) {
       merge(args[0], args[i]);
     }
     return args[0];
   };
-  
-  // Public
-  Breakpoints.prototype.addBreakpointHandler = 
-  Breakpoints.prototype.addEventListener = function(event, fn) {
-    this.handlers[event].push(fn);
-  };
-  
-  Breakpoints.prototype.fireBreakpoint = function(event){
-    var args = [].slice.call(arguments, 1), 
-        callbacks = this.handlers[event];
-  
-    if (callbacks) {
-      callbacks = callbacks.slice(0);
-      for (var i = 0, len = callbacks.length; i < len; ++i) {
-        callbacks[i].apply(this, args);
-      }
-    }
-  };
-
-  Breakpoints.defaults = {
-    breakpoints: [0, 768, 992, 1200],
-    timeout: false, // Store timeout id.
-    delay: 50, // Time to wait before the resize event is stopped.
-  };
 
   window.Breakpoints = Breakpoints;
-
 })();
-
-// Initilize Breakpoints object.
-var bp = new Breakpoints();
-
-// Attach resize listener to window
-window.addEventListener('resize', function() {
-  // Clear the timeout.
-  clearTimeout(bp.options.timeout);
-  
-  // Start timeout to wait for resize event to stop.
-  bp.options.timeout = setTimeout(function(){
-    bp.handleResize();
-  }, bp.options.delay);
-});
-
-bp.addBreakpointHandler('breakpoint0', function(){
-  console.log('mobile');
-});
-
-bp.addBreakpointHandler('breakpoint768', function(){
-  console.log('wide mobile, small tablet');
-});
-
-bp.addBreakpointHandler('breakpoint992', function(){
-  console.log('wide tablet');
-});
-
-bp.addBreakpointHandler('breakpoint1200', function(){
-  console.log('desktop');
-});
